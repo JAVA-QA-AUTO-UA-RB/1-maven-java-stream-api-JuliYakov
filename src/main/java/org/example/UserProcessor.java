435cb4b8f8c14e4dfef72b063eca0bb7fa57fc11
JuliYakov <145484982+JuliYakov@users.noreply.github.com>
2024-08-06 16:25:14 +0300

    Homework #12. Added implementation for static methods in the UserLoader and UserProcessor API classes.

--- src/main/java/org/example/UserProcessor.java
+++ src/main/java/org/example/UserProcessor.java
@@ -3,17 +3,24 @@ package org.example;
 import org.example.dto.User;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Comparator;
 
 public class UserProcessor {
 
     public static List<User> filterAndSortUsers(List<User> users) {
-        // Напишіть реалізацію метода, який повертає новий список.
-        // список, що повертається має містити тільки юзерів, у яких:
-        // а) вік більше 25 років
-        // б) Імейл закінчується на "ilovedogs.com" АБО "ilovecats.com"
-        // список, що повертається повинен бути відсортованим по полю "name" в алфавітному порядку Ascending (тобто від A -> Z)
-
-        //!!! У тілі метода ЗАБОРОНЕНО використовувати класичні цикли for, while, do-while для перебору по колекціям
-        return null;
+        return users.stream()
+                .filter(user -> user.getAge() > 25)
+                .filter(user -> user.getEmail().endsWith("ilovedogs.com") || user.getEmail().endsWith("ilovecats.com"))
+                .sorted(Comparator.comparing(User::getName))
+                .collect(Collectors.toList());
     }
 }
+
+// Напишіть реалізацію метода, який повертає новий список.
+// список, що повертається має містити тільки юзерів, у яких:
+// а) вік більше 25 років
+// б) Імейл закінчується на "ilovedogs.com" АБО "ilovecats.com"
+// список, що повертається повинен бути відсортованим по полю "name" в алфавітному порядку Ascending (тобто від A -> Z)
+
+//!!! У тілі метода ЗАБОРОНЕНО використовувати класичні цикли for, while, do-while для перебору по колекціям
